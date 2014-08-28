03477cf72265833ce6a88df7c1f698b8edffcd61
Nick D <ndepalma@gmail.com>
2014-08-28 14:17:20 -0400

    REady for pi

--- py/entry.py
+++ py/entry.py
@@ -5,6 +5,9 @@ import numpy.linalg
 import math
 import socket
 import sys
+import io
+import time
+import picamera
 
 
 class CalibrationMarker:
@@ -24,7 +27,7 @@ class CalibrationMarker:
             #print "got: "+ line + " ---> " + str(self.pos)
             return True
         return False
-        
+
 
 calibCap = None
 def MouseCalibrate(image, markers):
@@ -45,13 +48,13 @@ def MouseCalibrate(image, markers):
         #redisplay image and title
         cv.ShowImage(windowName, image);
         ind[0] = i
-        
+
         #ask for pt
         while not gotPoint[ind[0]]:
             ret, calibImage = calibCap.retrieve()
             cv2.imshow(windowName, calibImage);
             cv.WaitKey(1);
-            
+
         #add point to matrix
         markers[i].pos = [pt[0], pt[1]];
 
@@ -74,7 +77,7 @@ class Camera:
         self.blue = CalibrationMarker("BLUE");
         self.green = CalibrationMarker("GREEN");
         self.yellow = CalibrationMarker("YELLOW");
-        
+
         self.calibrationmarkers = [self.red, self.blue, self.green, self.yellow]
 
         if(type(id) == type('imastring')):
@@ -94,7 +97,7 @@ class Camera:
     def Save(self, f, prefix=""):
         for calib in self.calibrationmarkers:
             calib.Save(f, "CAM"+str(self.id))
-    
+
     def Load(self, line, prefix=""):
         iscalib = [calib.Load(line, "CAM"+str(self.id)) for calib in self.calibrationmarkers]
         self.LdCounter += iscalib.count(True)
@@ -122,25 +125,25 @@ class Camera:
         #B(0),B(1) = 0   #marker 1
         B[2] = 1; B[5]=1 #marker 2,3
         B[4] = 1;
-        B[6] = 0; B[7]=1 
+        B[6] = 0; B[7]=1
         #B[3] = 1; B[4]=1 #marker 2,3
         #B[6] = 1; B[7]=1 #marker 4
-        
-        
+
+
         #create our polygon
         self.px = numpy.array([-1, 8, 13, -4])
-        self.py = numpy.array([-1, 3, 11, 8])  
+        self.py = numpy.array([-1, 3, 11, 8])
         i = 0
         for calib in self.calibrationmarkers:
             self.px[(3+i)%4] = calib.pos[0]
             self.py[(3+i)%4] = -calib.pos[1]
             i = i + 1
-        
-        
+
+
         print "PX: \n", str(self.px)
         print "PY: \n", str(self.py)
         #compute coefficients
-        
+
         #A=numpy.mat('1 0 0 0;1 1 0 0;1 1 1 1;1 0 1 0')
         #print "A: \n", str(A)
         #AI = numpy.invert(A)
@@ -159,7 +162,7 @@ class Camera:
         #plot_internal_and_external_points(px,py,a,b);
 
         return self.a, self.b
-    
+
     # converts physical (x,y) to logical (l,m)
     def FrameCorrect(self, x, y):
         #quadratic equation coeffs, aa*mm^2+bb*m+cc=0
@@ -173,7 +176,7 @@ class Camera:
         #compute m = (-b+sqrt(b^2-4ac))/(2a)
         det = math.sqrt(bb*bb - 4*aa*cc);
         m = (-bb+det)/(2*aa);
-        
+
         #compute l
         l = (x-a[0,0]-a[0,2]*m)/(a[0,1]+a[0,3]*m)
         return l,m
@@ -208,26 +211,26 @@ def CompositeShow(windowName, camera, image, settings, pts=[]):
     if(Uncalibrated):
         CalibrateCameras(comp)
         Uncalibrated = False
-    
+
     if(settings.showGrid):
         #draw lines
 
         #p1 - p2
         cv.Line(comp, tuple(camera.calibrationmarkers[0].pos), \
                       tuple(camera.calibrationmarkers[1].pos), cv.Scalar(0, 255, 0), 1, cv.CV_AA)
-        
+
         #p1 - p4
         cv.Line(comp, tuple(camera.calibrationmarkers[0].pos), \
                       tuple(camera.calibrationmarkers[3].pos), cv.Scalar(0, 255, 0), 1, cv.CV_AA)
-                
+
         #p3 - p4
         cv.Line(comp, tuple(camera.calibrationmarkers[2].pos), \
                       tuple(camera.calibrationmarkers[3].pos), cv.Scalar(0, 255, 0), 1, cv.CV_AA)
-        
+
         #p2 - p3
         cv.Line(comp, tuple(camera.calibrationmarkers[1].pos), \
                       tuple(camera.calibrationmarkers[2].pos), cv.Scalar(0, 255, 0), 1, cv.CV_AA)
-    
+
     for pt in pts:
         cv.Circle(comp, (int(pt[0]), int(pt[1])), 3, cv.Scalar(255, 0, 0))
     cv.ShowImage(windowName, comp)
@@ -295,7 +298,7 @@ def unit2world(pos):
     world_y = numpy.asscalar(numpy.dot(g_b, unit.T))
     return (world_x, world_y)
 
-#test 
+#test
 #print "unit2world 0,0: \n", str(unit2world([0,0]))
 #print "unit2world 1,0: \n", str(unit2world([1,0]))
 #print "unit2world 0 1: \n", str(unit2world([0,1]))
@@ -307,18 +310,18 @@ def unit2world(pos):
 class Settings:
     def __init__(self):
         self.showGrid = True
-        
+
 settings = Settings()
 
 
 #Test 1
-##CompositeShow("Camera 1", cam1, settings) 
+##CompositeShow("Camera 1", cam1, settings)
 ##def mouseback_rect(event,x,y,flags,param):
 ##    if event==cv.CV_EVENT_LBUTTONUP:		# here event is left mouse button double-clicked
 ##        new = cam1.FrameCorrect(x,y)
 ##        print x,y, "->", new
-##        
-##        
+##
+##
 ##
 ##cv.SetMouseCallback("Camera 1", mouseback_rect);
 ##cv.WaitKey()
@@ -329,47 +332,47 @@ cv.NamedWindow("Image window", 1)
 FilterWindowName = "Filter Window "
 def nothing(da):
     pass
-    
+
 def setupGUI(tag, min_default=128, max_default=128):
     global FilterWindowName
     cv2.namedWindow(FilterWindowName+tag, 2)
     cv2.createTrackbar(tag+" Min", FilterWindowName+tag, min_default, 255, nothing)
     cv2.createTrackbar(tag+" Max", FilterWindowName+tag, max_default, 255, nothing)
-    
-    
+
+
 def FindBall(im2):
     global FilterWindowName
     hsv = cv2.cvtColor(im2, cv.CV_RGB2HSV)
     [h, s, v] = cv2.split(hsv)
-    
+
     #---------------------
     high_bnd = cv2.getTrackbarPos("Hue Max", FilterWindowName+"Hue")
     low_bnd = cv2.getTrackbarPos("Hue Min", FilterWindowName+"Hue")
-    
+
     ret, hi1 = cv2.threshold(h, high_bnd, 255, cv2.THRESH_BINARY_INV)
     ret, hi2 = cv2.threshold(h, low_bnd, 255, cv2.THRESH_BINARY)
-    
+
     hi = cv2.bitwise_and(hi1, hi2)
     cv2.imshow(FilterWindowName+"Hue", hi);
     #---------------------
     high_bnd = cv2.getTrackbarPos("Value Max", FilterWindowName+"Value")
     low_bnd = cv2.getTrackbarPos("Value Min", FilterWindowName+"Value")
-    
+
     ret, vi1 = cv2.threshold(v, high_bnd, 255, cv2.THRESH_BINARY_INV)
     ret, vi2 = cv2.threshold(v, low_bnd, 255, cv2.THRESH_BINARY)
-    
+
     vi = cv2.bitwise_and(vi1, vi2)
     cv2.imshow(FilterWindowName+"Value", vi);
     #---------------------
     out = cv2.bitwise_and(vi, hi)
 
-    
+
     return out
 
 def ErodeTrick(im):
     cv2.erode(im, im, None, 3)
     cv2.dilate(im, im, None, 3)
-    
+
     cv2.dilate(im, im, None, 3)
     cv2.erode(im, im, None, 3)
     return im
@@ -387,7 +390,7 @@ def PickBlob(im):
         ara = cv2.contourArea(cntr)
         if(low_band < ara < high_bnd):
             cntrs.append(cntr)
-    
+
     centroids = []
     for cntr in cntrs:
         mu = cv2.moments(cntr)
@@ -398,14 +401,14 @@ def PickBlob(im):
     return centroids
 
 
-cap = cv2.VideoCapture(-1)
-cap.set(cv.CV_CAP_PROP_FRAME_WIDTH , 320); 
-cap.set(cv.CV_CAP_PROP_FRAME_HEIGHT , 240);
-cap.set(cv.CV_CAP_PROP_FPS, 20); 
-cap.set(cv.CV_CAP_PROP_FOURCC, cv.CV_FOURCC('R', 'G', 'B', '3'))
-for i in range(0,20):
-    ret, cv_image = cap.retrieve()
-    cv.WaitKey(100)
+#cap = cv2.VideoCapture(-1)
+#cap.set(cv.CV_CAP_PROP_FRAME_WIDTH , 320);
+#cap.set(cv.CV_CAP_PROP_FRAME_HEIGHT , 240);
+#cap.set(cv.CV_CAP_PROP_FPS, 20);
+#cap.set(cv.CV_CAP_PROP_FOURCC, cv.CV_FOURCC('R', 'G', 'B', '3'))
+#for i in range(0,20):
+#    ret, cv_image = cap.retrieve()
+#    cv.WaitKey(100)
 
 class TCPClient:
     def __init__(self, Address, Port):
@@ -424,12 +427,12 @@ class TCPClient:
         try:
             i = 0
             for pt in pts:
-                # the data should be sent as 3 comma separated values with a total length of 19 characters. 
+                # the data should be sent as 3 comma separated values with a total length of 19 characters.
                 # 1 for id, 8 for x, 8 for y
                 # [ i,xxxxxxxx,yyyyyyyy ]
                 message = '' + str(i) + ',' + ("%8f" % pt[0]) + ',' + ("%8f" % pt[1])
 
-                # Send data            
+                # Send data
                 sock.sendall(message)
                 i = i + 1
                 if(i == 10):
@@ -437,11 +440,25 @@ class TCPClient:
         except:
             pass
 
-    
+
 tcpc = TCPClient('localhost', 4558)
 
+class RaspiCap:
+    def __init__(self):
+        stream = self.stream = io.BytesIO()
+        camera = self.camera = picamera.PiCamera()
+        camera.start_preview()
+        time.sleep(2)
+        camera.capture(stream, format='jpeg')
+    def __retrieve():
+        # Construct a numpy array from the stream
+        data = np.fromstring(self.stream.getvalue(), dtype=np.uint8)
+        # "Decode" the image from the array, preserving colour
+        image = cv2.imdecode(data, 1)
+        return image
 
-calibCap = cap    
+cap = RaspiCap()
+calibCap = cap
 def image_call():
     global FilterWindowName
     ret, cv_image = cap.retrieve()
@@ -453,20 +470,20 @@ def image_call():
             cv2.circle(cv_image, (int(mc[0]), int(mc[1])), 3, cv.Scalar(255, 0, 0))
         #cv_image = cv2.cvtColor(cv_image, cv.CV_BGR2RGB)
         cvIm = cv.CreateImageHeader((cv_image.shape[1], cv_image.shape[0]), cv.IPL_DEPTH_8U, 3)
-        cv.SetData(cvIm, cv_image.tostring(), 
+        cv.SetData(cvIm, cv_image.tostring(),
                cv_image.dtype.itemsize * 3 * cv_image.shape[1])
         CompositeShow("Image window", cam1, cvIm, settings)
-        #correct the frame 
+        #correct the frame
         tosend = []
         for mc in mcs:
             new = cam1.FrameCorrect(mc[0],mc[1])
-            
+
             rectified = unit2world(new)
-            
+
             #print "Ball: ", str(mc), " -> ", str(new)
             #print "Ball: ", str(mc), " -> ", str(rectified)
             tosend.append(rectified)
-        
+
         #now send it!
         tcpc.send(tosend)
 
@@ -489,7 +506,7 @@ while(True):
         NeedsToSave = False
 cv.DestroyAllWindows()
 
-   
+
 
 
 #play all until one dies
@@ -497,15 +514,15 @@ cv.DestroyAllWindows()
 #      im3 != None and im4 != None):
     # color thresh video
     # -overlay transparent
-    
-    # + mask 
+
+    # + mask
     # +erode, dilate, flip
-    
+
     # + overlay tracking transparent
-    
+
 
 # + all of these filters as GUI
 # + all of these layers as GUI
-# + affine - two cameras    
-    
+# + affine - two cameras
+
 # fake distances to get cm accuracy
