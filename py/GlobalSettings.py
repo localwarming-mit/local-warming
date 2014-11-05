dc683e52a06eb16a38c224006085f925f4c24219
cgreaves <cgreaves91@gmail.com>
2014-11-05 09:21:25 -0500

    added a few other variables to make setup easier

--- py/GlobalSettings.py
+++ py/GlobalSettings.py
@@ -4,3 +4,20 @@ cappi=False
 showGrid = True
 
 FilterWindowName = "Filter Window "
+
+# quickly set which times the arrays will run which methods
+
+trackingTime = '18:00:00'
+waveTime = '17:30:00'
+spotsTime = '17:00:00'
+
+# set whether pi is client or server
+
+TCPServer = True
+TCPClient = False
+
+# set IP adress and partner IP adress
+
+IP_Address = '192.168.1.1'
+
+
