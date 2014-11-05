e218f54bbbcf5a9394fe2b54dde862caa33c29e2
cgreaves <cgreaves91@gmail.com>
2014-11-05 09:17:37 -0500

    Update timestamp.py

--- py/timestamp.py
+++ py/timestamp.py
@@ -2,7 +2,7 @@ import time
 import datetime
 
 def main():
-    print hour()
+    print clock()
 
 # return a date and time from the computer clock
 
@@ -16,6 +16,12 @@ def timestamp():
 # split(" ",1) will split a string into an array containing the strings
 # before the selected character and after the character
 
+def clock():
+    stamp = timestamp()
+    stamp_list = stamp.split(" ",1)
+    time = stamp_list[1]
+    return time
+
 def hour():
     stamp = timestamp()
     stamp_list = stamp.split(" ",1)
@@ -23,7 +29,7 @@ def hour():
     hour = time.split(":",1)
     return hour[0]
 
-# parse a time stamp first into the time, and then into the minute
+# parse a time stamp first into the time, and then into the hour
 
 def minute():
     stamp = timestamp()
@@ -32,6 +38,15 @@ def minute():
     minsec = time.split(":",1)
     minute = minsec[1].split(":",1)
     return minute[0]
+
+def second():
+    stamp = timestamp()
+    stamp_list = stamp.split(" ",1)
+    time = stamp_list[1]
+    minsec = time.split(":",1)
+    second = minsec[1].split(":",1)
+    return second[1]
     
 if __name__ == "__main__":
     main()
+
